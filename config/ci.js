017bb5d95107faa9cd11fb104d84ddc30b30b4b4
slavkozivancevic <98930167+slavkozivancevic@users.noreply.github.com>
2023-10-22 13:11:47 +0200

    Update ci.js

--- config/ci.js
+++ config/ci.js
@@ -1,9 +1,9 @@
 module.exports = {
   googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+    process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI:
     "mongodb://127.0.0.1:27017/blog_ci",
-  cookieKey: "123123123",
+  cookieKey: process.env.COOKIE_KEY,
   redisUrl: "redis://127.0.0.1:6379"
 };
